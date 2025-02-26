dd314e11b47048f5be55054565a3c6583ad8643a
Coder-himank <gandivisbestbow@gmail.com>
2025-02-26 21:39:14 +0530

    user profile changes 1

--- pages/profile.jsx
+++ pages/profile.jsx
@@ -6,6 +6,8 @@ import styles from "@/styles/userProfile.module.css";
 import axios from "axios";
 import { signOut } from "next-auth/react";
 import UnAuthorizedUser from "@/components/UnAuthorizedUser";
+import Link from "next/link";
+import { FaBars } from "react-icons/fa";
 
 export default function UserProfile() {
   const { data: session } = useSession();
@@ -13,7 +15,13 @@ export default function UserProfile() {
 
   const [user, setUser] = useState(null);
   const [error, setError] = useState(null);
+  const [toogleBars, setToogleBars] = useState(false)
   const sectionsRef = useRef([]);
+
+  const aside_styles = {
+    left: "0%",
+    padding: "80px 10px 0 0"
+  }
 
   // Fetch user profile
   useEffect(() => {
@@ -62,22 +70,41 @@ export default function UserProfile() {
     <>
       <div className="navHolder"></div>
       <div className={styles.profile_container}>
-
         {
           session?.user
             ? <>
+              <aside className={styles.aside} style={toogleBars ? aside_styles : {}}>
+                <Link href={"/profile"}>Dashboard</Link>
+                <Link href={"/refund"}>Refund</Link>
+                <Link href={"/track-order"}>Track Order</Link>
+                <Link href={"/payment-history"}>Payemnt History</Link>
+                <Link href={"/customer-care"}>Customer Care</Link>
+                <Link href={"/help"}>Help</Link>
+              </aside>
 
-              <div><span>{user?.name}</span> <span>{user?.loyaltyPoints}</span></div>
 
               <div className={styles.profile_card}>
-                <table>
+                <div className={styles.top_name}>
+                  <div className={styles.top_name_left}>
+                    <button className={styles.bars} onClick={() => setToogleBars((prev) => !prev)}><FaBars /></button>
+                    <span className={styles.username}>{user?.name}</span>
+
+                  </div>
+                  <div>
+                    <span className={styles.loyaltyPoint}>{user?.loyaltyPoints}</span></div>
+                </div>
+                <table className={styles.user_detail}>
+                  <tr>
+                    <td>{t("userId")}</td>
+                    <td>{user?._id}</td>
+                  </tr>
                   <tr>
                     <td>{t("email")}</td>
                     <td>{user?.email}</td>
                   </tr>
                   <tr>
 
-                    <td>{t("phone")}:</td>
+                    <td>{t("phone")}</td>
                     <td>{user?.phone}</td>
                   </tr>
 
@@ -85,32 +112,49 @@ export default function UserProfile() {
                 <section>
                   <h3>Addresses</h3>
 
-                  {user.addresses.map((item, index) => (
-                    <div>
-                      <span>{item.lable}</span>
-                      <span>{item.address}</span>
-                      <span>{item.city}</span>
-                      <span>{item.country}</span>
-                      <span>{item.pincode}</span>
-                    </div>
+                  {user?.addresses.map((item, index) => (
+                    <table className={styles.address_plate}>
+                      <tr>
+                        <td>{item.address}</td>
+                        <td>{item.city}</td>
+                        <td>{item.country}</td>
+                        <td>{item.lable || "-"}</td>
+                        <td>{item.pincode}</td>
+                      </tr>
+                    </table>
                   ))}
+                </section>
+                <section className={styles.orders}>
+                  <h3>Orders</h3>
+                  <Link href={"/track-orders"}>Track Orders</Link>
+                  <Link href={"/orders?show=canceled"}>Canceld Orders</Link>
+                  <Link href={"/orders"}>All Orders</Link>
                 </section>
                 <section>
                   <h3>Cart</h3>
-                  {user.cartData.map((item) => (
-                    <>
-                      {item.productId} ----
-                      {item.quantity_demanded}
-                    </>
-                  ))}
+                  <div>
+
+                    {user.cartData.map((item) => (
+                      <>
+                        <div className={styles.productCard}>
+                          {item.productId} ---- {item.quantity_demanded}
+                        </div>
+                      </>
+                    ))}
+                  </div>
                 </section>
                 <section>
                   <h3>Wishlist</h3>
-                  {user.wishlistData.length !== 0 ? user.wishlistData.map((item) => (
-                    <>
-                      {item.productId}
-                    </>
-                  )) : <p>Add some products to wishlist</p>}
+                  <div>
+
+                    {user.wishlistData.length !== 0 ? user.wishlistData.map((item) => (
+                      <>
+                        <div className={styles.productCard}>
+                          {item.productId}
+                        </div>
+                      </>
+                    )) : <p>Add some products to wishlist</p>}
+                  </div>
                 </section>
 
                 <section>
