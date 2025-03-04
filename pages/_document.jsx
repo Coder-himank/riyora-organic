43af449b7a6ed64e9182f056fc95f4b6cd35e227
Coder-himank <gandivisbestbow@gmail.com>
2025-03-05 03:59:59 +0530

    google search console verification"

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-
+        <meta name="google-site-verification" content="B0jup2cuRtIDQWWREl9IzbM9uEiqZYULkpf2HLerssY" />
         <script
           src="https://checkout.razorpay.com/v1/checkout.js"
           async
