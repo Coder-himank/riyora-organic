203876a0d717da6bc681f0b9e96ae7b31c71ac85
Coder-himank <gandivisbestbow@gmail.com>
2025-03-06 01:54:39 +0530

    html lang dynamic

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -4,9 +4,15 @@ import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import { SessionProvider } from "next-auth/react";
 import i18nConfig from "../next-i18next.config";
+import Head from "next/head";
+import { useRouter } from "next/router";
 function MyApp({ Component, pageProps }) {
+  const router = useRouter()
   return (
     <>
+      <Head>
+        <html lang={router.locale} />
+      </Head>
       <SessionProvider>
         <Navbar />
 
