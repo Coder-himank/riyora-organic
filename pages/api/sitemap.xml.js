d00546da3e25e8827a71c672965a6e3add75ab46
Coder-himank <gandivisbestbow@gmail.com>
2025-06-17 17:09:05 +0530

    Sitemap Changed

--- pages/api/sitemap.xml.js
+++ pages/api/sitemap.xml.js
@@ -7,7 +7,7 @@ export default async function handler(req, res) {
         const baseUrl = process.env.BASE_URL || "https://organic-robust.vercel.app";
 
         // Supported languages for internationalization (i18n)
-        const locales = ["en", "hi"];
+        const locales = ["en"];
 
         // Connect to MongoDB
         await connectDB();
@@ -28,8 +28,6 @@ export default async function handler(req, res) {
             "products",
             "privacy-policy",
             "services",
-            "cart",
-            "wishlist",
             "authenticate"
         ];
 
@@ -38,51 +36,28 @@ export default async function handler(req, res) {
         <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" 
                 xmlns:xhtml="http://www.w3.org/1999/xhtml">
             
-            ${staticPages
-                .map((page) =>
-                    locales
-                        .map(
-                            (locale) => `
-                                <url>
-                                    <loc>${baseUrl}/${locale}/${page}</loc>
+            ${staticPages.map((page) =>
+            `<url>
+                                    <loc>${baseUrl}/${page}</loc>
                                     <lastmod>${new Date().toISOString()}</lastmod>
-                                    ${locales
-                                    .map(
-                                        (altLang) => `<xhtml:link 
-                                                    rel="alternate" 
-                                                    hreflang="${altLang}" 
-                                                    href="${baseUrl}/${altLang}/${page}" />`
-                                    )
-                                    .join("")}
+ 
                                 </url>
                             `
-                        )
-                        .join("")
-                )
+
+        )
                 .join("")}
 
             ${products
                 .map((product) =>
-                    locales
-                        .map(
-                            (locale) => `
+                    `
                                 <url>
-                                    <loc>${baseUrl}/${locale}/products/${product._id.toString()}</loc>
+                                    <loc>${baseUrl}/products/${product._id.toString()}</loc>
                                     <lastmod>${new Date(product.updatedAt || Date.now()).toISOString()}</lastmod>
                                     <changefreq>weekly</changefreq>
                                     <priority>0.8</priority>
-                                    ${locales
-                                    .map(
-                                        (altLang) => `<xhtml:link 
-                                                    rel="alternate" 
-                                                    hreflang="${altLang}" 
-                                                    href="${baseUrl}/${altLang}/products/${product._id.toString()}" />`
-                                    )
-                                    .join("")}
+                                    
                                 </url>
                             `
-                        )
-                        .join("")
                 )
                 .join("")}
         </urlset>`;
