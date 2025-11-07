85e089f554975c54ef55a5752201e9a320f17c74
Coder-himank <gandivisbestbow@gmail.com>
2025-11-07 17:40:59 +0530

    google analytics tags@

--- middleware.js
+++ middleware.js
@@ -89,7 +89,7 @@ export async function middleware(req) {
     style-src 'self' 'unsafe-inline' https://fonts.googleapis.com;
     font-src 'self' https://fonts.gstatic.com;
     connect-src 'self' https://api.razorpay.com https://lumberjack.razorpay.com https://checkout.razorpay.com https://www.googletagmanager.com https://www.google-analytics.com;
-    frame-src frame-src 'self' https://checkout.razorpay.com https://api.razorpay.com https://www.googletagmanager.com;
+    frame-src 'self' https://checkout.razorpay.com https://api.razorpay.com https://www.googletagmanager.com;
 ;
     object-src 'none';
     base-uri 'self';
